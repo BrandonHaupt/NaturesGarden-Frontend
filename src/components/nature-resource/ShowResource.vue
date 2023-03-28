1724f7d33238f86053beb10ed881a3975615a35c
Brandon Haupt <haupt.brandon@gmail.com>
2023-03-28 18:39:24 -0500

    changed up show page and added a toggle button to show/hide the edit form

--- src/components/nature-resource/ShowResource.vue
+++ src/components/nature-resource/ShowResource.vue
@@ -7,9 +7,13 @@
       </section>
       <section class="item-info">
         <h1>{{ item.name }}</h1>
+        <h3>Native To:</h3>
         <p>{{ item.origin }}</p>
+        <h3>Is it edible?</h3>
         <p>{{ item.edible }}</p>
+        <h3>Benefits</h3>
         <p>{{ item.benefits }}</p>
+        <h3>Description:</h3>
         <p>{{ item.description }}</p>
       </section>
      
@@ -19,32 +23,34 @@
       <h1>Loading...</h1>
     </div>
 
-    <form class="form-show" @submit.prevent="submitForm">
-      <!-- <label for="name">Resource Name: </label> -->
-        <input type="text" id="name" name="name" v-model.trim="item.name" placeholder="Resource Name">
+    <button class="toggleDetailsButton" @click="toggleDetails">{{detailsAreVisible ? 'Hide' : 'Edit'}}</button>
+      <ul v-if="detailsAreVisible">
+        <form class="form-show" @submit.prevent="submitForm">
+        <!-- <label for="name">Resource Name: </label> -->
+          <input type="text" id="name" name="name" v-model.trim="item.name" placeholder="Resource Name">
 
-        <!-- If it is edible -->
-        <input type="text" id="edible" name="edible" v-model="item.edible" placeholder="is it edible?">
-        
-        <!-- origin -->
-        <input type="text" id="origin" name="origin" v-model.trim="item.origin" placeholder="Where is the plant Native to?">
+          <!-- If it is edible -->
+          <input type="text" id="edible" name="edible" v-model="item.edible" placeholder="is it edible?">
+          
+          <!-- origin -->
+          <input type="text" id="origin" name="origin" v-model.trim="item.origin" placeholder="Where is the plant Native to?">
 
-        <!-- URL -->
-        <input type="text" id="url" name="url" v-model.trim="item.url" placeholder="Enter image address">
+          <!-- URL -->
+          <input type="text" id="url" name="url" v-model.trim="item.url" placeholder="Enter image address">
 
-        <!-- Benefits -->
-        <textarea name="benefits" id="benefits" cols="30" rows="1" placeholder="Enter the benefits of the plant" v-model="item.benefits" />
+          <!-- Benefits -->
+          <textarea name="benefits" id="benefits" cols="30" rows="1" placeholder="Enter the benefits of the plant" v-model="item.benefits" />
 
-        <!-- description -->
-        <textarea name="description" id="description" cols="30" rows="1" placeholder="Enter the description of the plant" v-model="item.description" />
+          <!-- description -->
+          <textarea name="description" id="description" cols="30" rows="1" placeholder="Enter the description of the plant" v-model="item.description" />
 
-        <p v-if="invalidInput">One or more input fields are invalid. Please check your provided data.</p>
-        <p v-if="error">{{error}}</p>
-        
-        <button>Submit</button>
-        <div class="form-submit-button">
-        </div>
-    </form>
+          <p v-if="invalidInput">One or more input fields are invalid. Please check your provided data.</p>
+          <p v-if="error">{{error}}</p>
+          
+          <button>Submit</button>
+        </form>
+      </ul>
+    
   </section>
 
 </template>
@@ -57,7 +63,8 @@ export default {
 
   data() {
     return {
-      item: null
+      item: null,
+      detailsAreVisible: false,
     };
   },
   
@@ -106,7 +113,11 @@ export default {
       .catch(error => {
         console.error('Error updating nature data:', error);
       });
-    }
+    },
+
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible
+    },
   }
 };
 </script>
@@ -116,6 +127,19 @@ export default {
     height: 700px;
     width: 700px;
     object-fit: cover;
+    border-radius: 20px;
+  }
+
+  p {
+    padding-bottom: 1.5rem;
+  }
+
+  .toggleDetailsButton {
+    padding: 5px 20px;
+  }
+
+  .toggleDetailsButton:hover, .form-show button:hover{
+    cursor: pointer;
   }
 
   .loading {
@@ -180,12 +204,13 @@ export default {
 
 @media (min-width: 80em){
   .show-resource-page{
-    padding: 10px;
+    margin-top: -5rem;
   }
 
   .main-show-container{
     display: flex;
     flex-direction: row;
+    justify-content: center;
     padding: 0 0 2rem 0;
   }
 
@@ -197,6 +222,24 @@ export default {
   .form-show {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
+    width: 50%;
+    margin: 0 auto;
+    padding-bottom: 2rem;
+  }
+  .form-show input, .form-show textarea {
+    margin: 5px;
+  }
+
+  /* .form-show button:hover {
+    cursor: pointer;
+  } */
+
+  .toggleDetailsButton {
+    margin: 0 0 2rem 8rem;
+  }
+
+  .toggleDetailsButton:hover{
+    cursor: pointer;
   }
 
 }
